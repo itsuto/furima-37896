b3b87994238d405e59eab39365099966f0691594
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-05-12 23:22:58 +0900

    文字制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "全角のみで入力して下さい"}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "全角のみで入力して下さい"}
+  validates :last_name_kana, presence: true,format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみで入力して下さい"}
+  validates :first_name_kana, presence: true,format: {with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみで入力して下さい"}
   validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "半角英数を両方含む必要があります"}
+
 end
