d5ffdd19435e8e051d384df0be75fe4bc15c6dc6
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-05-13 11:04:08 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  t.string :name, null: false
+  t.text :info, null: false
+  t.integer :category_id, null: false
+  t.integer :condition_id, null: false
+  t.integer :delibery_burden_id, null: false
+  t.integer :prefecture_id, null: false
+  t.integer :date_id, null: false
+  t.integer :price, null: false
+  t.references :user, null: false, foreign_key: true
 end
