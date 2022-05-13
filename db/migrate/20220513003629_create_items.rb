d6b4b891554b2888d3a2cf35d48e58775d06155f
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-05-13 14:42:50 +0900

    Activehash

--- db/migrate/20220513003629_create_items.rb
+++ db/migrate/20220513003629_create_items.rb
@@ -7,7 +7,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :condition_id, null: false
       t.integer :delibery_burden_id, null: false
       t.integer :prefecture_id, null: false
-      t.integer :date_id, null: false
+      t.integer :shipping_date_id, null: false
       t.integer :price, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
