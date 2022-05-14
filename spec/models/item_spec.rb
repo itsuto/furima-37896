9b330f4dcdcc985a040fc57d5ec4186e860ed29f
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-05-14 21:49:15 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -52,6 +52,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank") 
       end
+      it 'priceが全角数字では登録できない' do
+        @item.price = "１２３４"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
       it 'nameが41文字以上では保存できない' do
         @item.name = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab'
         @item.valid?
