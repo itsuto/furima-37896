c2b719ccd04bfee742f51a2ee8e4979236068b45
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-05-15 19:41:01 +0900

    「imageが空」テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -82,6 +82,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("User must exist")
       end
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
     end
   end
 end
