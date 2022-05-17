38b1fba310be46b7519b7024811bda9736705e6a
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-05-17 15:44:42 +0900

    商品詳細

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :delibery_burden
-  belongs_to_active_hash :Prefecture
+  belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_date
 
   validates :name, presence: true, length: { maximum: 40 }
