893f045c362484adc31e98e71d802fe0c33eb33f
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-05-18 19:31:09 +0900

    コントローラー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,23 @@
 class OrdersController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order = OrderDestination.new(order_params)
+    if @order.valid? 
+      @order.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
