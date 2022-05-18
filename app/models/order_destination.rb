a8b6910f098e2b82331b3d224e4ef5e21b58d59c
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-05-18 19:30:55 +0900

    ルーティング

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -6,7 +6,7 @@ class OrderDestination
     validates :user_id
     validates :item_id
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, , numericality: {other_than: 1, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :block
     validates :phone_number
