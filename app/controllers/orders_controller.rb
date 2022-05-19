0f5744b7eab71680bf706254107eb4b2670569f4
itsuto <p-haya.p.p.8810@docomo.ne.jp>
2022-05-19 10:55:50 +0900

    保存記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,12 +2,13 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_destination = OrderDestination.new
   end
 
   def create
-    @order = OrderDestination.new(order_params)
-    if @order.valid? 
-      @order.save
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid? 
+      @order_destination.save
       redirect_to root_path
     else
       @item = Item.find(params[:item_id])
@@ -18,6 +19,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_destination).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -6,11 +6,11 @@ class OrderDestination
     validates :user_id
     validates :item_id
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :block
     validates :phone_number
   end
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
